f9274daba603b65c56476c79559c9e6cac382c36
cquhahei <863137071@qq.com>
2024-08-25 09:30:38 +0800

    分配宿舍功能

--- src/main/java/com/test/trainingprogrambackend/entity/Dormitory.java
+++ src/main/java/com/test/trainingprogrambackend/entity/Dormitory.java
@@ -1,15 +1,24 @@
 package com.test.trainingprogrambackend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
-@Data
+import java.util.List;
+
+@Data // 可能会有问题，再看
+@TableName("student")
 @ApiModel("这是Dormitory实体类")
 public class Dormitory {
     @TableId("dorName")
     private String dorName;
     private String classes;
     private String deptName;
+    private short peoNumber;
+    private short isFull;
 
+    @TableField(exist = false)
+    private List<Student> students;//学生姓名
 }
