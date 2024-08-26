294167661e38e0e0d4fd9e6caf1670c079385f0b
cquhahei <863137071@qq.com>
2024-08-26 16:11:38 +0800

    增加注释和参数注解

--- src/main/java/com/test/trainingprogrambackend/controller/BackstageController.java
+++ src/main/java/com/test/trainingprogrambackend/controller/BackstageController.java
@@ -4,8 +4,10 @@ import com.test.trainingprogrambackend.entity.StudentStatistics;
 import com.test.trainingprogrambackend.mapper.DormitoryMapper;
 import com.test.trainingprogrambackend.mapper.StudentMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -14,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @Api("后台管理系统数据")
+@RequestMapping("/backstage")
 public class BackstageController {
     @Autowired
     private StudentMapper studentMapper;
@@ -22,13 +25,15 @@ public class BackstageController {
     private DormitoryMapper dormitoryMapper;
 
     //返回完成账号激活、选课总人数、选择申请宿舍、信息录入、注册以及总学生数，用于进度展示
-    @GetMapping("/backstage/process")
+    @ApiOperation("返回完成账号激活、选课总人数、选择申请宿舍、信息录入、注册以及总学生数，用于进度展示")
+    @GetMapping("/process")
     public StudentStatistics registrationNum() {
         return studentMapper.findAllStatistics();
     }
 
     // 用于中心旭日图，各班级完成注册人数、各专业实际人数、各学院实际人数
-    @GetMapping("/backstage/sunChart")
+    @ApiOperation("用于中心旭日图，各班级完成注册人数、各专业实际人数、各学院实际人数")
+    @GetMapping("/sunChart")
     public Map<String, Object> sunChartNum() {
         Map<String, Object> statSumChat = new HashMap<>();
         statSumChat.put("registeredNumByClass", studentMapper.countRegisteredNumByClass());
@@ -39,7 +44,8 @@ public class BackstageController {
 
 
     // 用于宿舍情况展示，各园区床位总数和入住人数统计情况
-    @GetMapping("/backstage/dorSituation")
+    @ApiOperation("用于宿舍情况展示，各园区床位总数和入住人数统计情况")
+    @GetMapping("/dorSituation")
     public Map<String, Object> dorSituation() {
         Map<String, Object> dorBedNum = new HashMap<>();
         dorBedNum.put("totalBedsByArea", dormitoryMapper.countTotalBedsByArea());
--- src/main/java/com/test/trainingprogrambackend/controller/DormitoryController.java
+++ src/main/java/com/test/trainingprogrambackend/controller/DormitoryController.java
@@ -6,10 +6,7 @@ import com.test.trainingprogrambackend.util.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Dictionary;
 import java.util.List;
@@ -22,34 +19,34 @@ public class DormitoryController {
     private DormitoryService dormitoryService;
 
     // 后台管理系统接口
-    @GetMapping("/assignDor")
     @ApiOperation("后台分配宿舍并返回分配结果")
+    @GetMapping("/assignDor")
     public List<Dormitory> assignDor(){
         return dormitoryService.assignDor();
     }
 
-    @GetMapping("/getNotFullDorByClasses")
     @ApiOperation("后台获取对应班级未满宿舍信息")
-    public List<Dormitory> getNotFullDorByClasses(String classes){
+    @GetMapping("/getNotFullDorByClasses")
+    public List<Dormitory> getNotFullDorByClasses(@RequestParam String classes){
         return dormitoryService.findNotFullDorByClasses(classes);
     }
 
-    @GetMapping("/refreshDor")
     @ApiOperation("后台刷新宿舍信息")
+    @GetMapping("/refreshDor")
     public List<Dormitory> refreshDor(){
         return dormitoryService.refreshDor();
     }
 
-    @PostMapping("/adjustByNotFullDor")
     @ApiOperation("后台调整学生至非满宿舍")
-    public Result adjustByNotNullDor(String studentid, String dorName){
+    @PostMapping("/adjustByNotFullDor")
+    public Result adjustByNotNullDor(@RequestParam String studentid, @RequestParam String dorName){
         dormitoryService.adjustDorByNotFullDor(studentid, dorName);
         return Result.ok().message("调整成功");
     }
 
-    @PostMapping("/adjustByExchange")
     @ApiOperation("后台交换学生宿舍")
-    public Result adjustByExchange(String studentid1, String studentid2) {
+    @PostMapping("/adjustByExchange")
+    public Result adjustByExchange(@RequestParam String studentid1, @RequestParam String studentid2) {
         dormitoryService.adjustDorByExchange(studentid1, studentid2);
         return Result.ok().message("交换成功");
     }
@@ -57,13 +54,13 @@ public class DormitoryController {
 
     @ApiOperation("学生是否申请宿舍")
     @PostMapping("/isApply")
-    public Result isApply(String studentid, int dorStatus){
+    public Result isApply(@RequestParam String studentid, @RequestParam int dorStatus){
         return dormitoryService.isApplyDor(studentid, dorStatus) == 1 ? Result.ok().message("选择是否申请成功") : Result.error().message("选择是否申请成功失败");
     }
 
-    @PostMapping("/viewDor")
     @ApiOperation("学生查看宿舍信息")
-    public Dormitory viewDor(String studentid){
+    @GetMapping("/viewDor")
+    public Dormitory viewDor(@RequestParam String studentid){
         return dormitoryService.findDorByStudentId(studentid);
     }
 }
