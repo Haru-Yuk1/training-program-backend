cf425601d4d2f96b2ead073c6a4a6198c65e20ba
HaruYuki <1366401799@qq.com>
2024-08-28 09:11:23 +0800

    8/27 9：10 课程管理2

---
+++ src/main/java/com/test/trainingprogrambackend/entity/Department.java
@@ -0,0 +1,16 @@
+package com.test.trainingprogrambackend.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("department")
+public class Department {
+    private String deptName;
+    private String building;
+}
